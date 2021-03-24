ae551ed3c591ad19eb97014d17c2ec374d6692c1
“Gabriel <gmaciasbaldovino02@gmail.com>
2021-03-24 00:15:06 -0500

    Implementación de ejercicio 1 punto a

--- taller-vectores.py
+++ taller-vectores.py
@@ -19,3 +19,41 @@ mayor = max(contenedor)
 
 print(f'Los elementos del vector son: {contenedor}')
 print(f'El elemento mayor es: {mayor}')
+
+# Punto 1:
+# d (El Mayor elemento)
+
+c_numeros = int(input('Digite la cantidad de numeros para el vector: '))
+contenedor = []
+menor = 0
+i = 1
+
+while (c_numeros > 0):
+    nums = int(input(f'Digite el numero {i} para el vector: '))
+    contenedor.append(nums)
+    i = i + 1
+    c_numeros = c_numeros - 1
+
+menor = min(contenedor)
+
+print(f'Los elementos del vector son: {contenedor}')
+print(f'El elemento menor es: {menor}')
+
+# Punto 1:
+# d (El Mayor elemento)
+
+c_numeros = int(input('Digite la cantidad de numeros para el vector: '))
+contenedor = []
+suma = 0
+i = 1
+
+while (c_numeros > 0):
+    nums = int(input(f'Digite el numero {i} para el vector: '))
+    contenedor.append(nums)
+    i = i + 1
+    c_numeros = c_numeros - 1
+
+suma = sum(contenedor)
+
+print(f'Los elementos del vector son: {contenedor}')
+print(f'La sumatoria de los elementos es: {suma}')
