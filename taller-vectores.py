8a57f01d3b94a21db1125d8a7fb9449356042ea8
“Gabriel <gmaciasbaldovino02@gmail.com>
2021-03-24 00:25:10 -0500

    Implementación de ejercicio 1 punto b

--- taller-vectores.py
+++ taller-vectores.py
@@ -3,6 +3,7 @@
 
 # Punto 1:
 # C (El Mayor elemento)
+import numpy as np
 
 c_numeros = int(input('Digite la cantidad de numeros para el vector: '))
 contenedor = []
@@ -21,7 +22,7 @@ print(f'Los elementos del vector son: {contenedor}')
 print(f'El elemento mayor es: {mayor}')
 
 # Punto 1:
-# d (El Mayor elemento)
+# d (El menor elemento)
 
 c_numeros = int(input('Digite la cantidad de numeros para el vector: '))
 contenedor = []
@@ -40,7 +41,7 @@ print(f'Los elementos del vector son: {contenedor}')
 print(f'El elemento menor es: {menor}')
 
 # Punto 1:
-# d (El Mayor elemento)
+# a (La sumatoria)
 
 c_numeros = int(input('Digite la cantidad de numeros para el vector: '))
 contenedor = []
@@ -57,3 +58,22 @@ suma = sum(contenedor)
 
 print(f'Los elementos del vector son: {contenedor}')
 print(f'La sumatoria de los elementos es: {suma}')
+
+# Punto 1:
+# b (La productoria)
+
+c_numeros = int(input('Digite la cantidad de numeros para el vector: '))
+contenedor = []
+producto = 0
+i = 1
+
+while (c_numeros > 0):
+    nums = int(input(f'Digite el numero {i} para el vector: '))
+    contenedor.append(nums)
+    i = i + 1
+    c_numeros = c_numeros - 1
+
+producto = np.prod(contenedor)
+
+print(f'Los elementos del vector son: {contenedor}')
+print(f'La productoria de los elementos es: {producto}')
